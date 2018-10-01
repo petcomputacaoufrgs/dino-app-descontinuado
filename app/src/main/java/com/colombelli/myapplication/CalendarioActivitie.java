fbbecee0a473ad0733a3f528c498dd1b1ea6abfa
ppons <ph.pfiorentin@gmail.com>
2018-10-01 09:58:17 -0300

    First Commit

---
+++ app/src/main/java/com/colombelli/myapplication/CalendarioActivitie.java
@@ -0,0 +1,13 @@
+package com.colombelli.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CalendarioActivitie extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendario_activitie);
+    }
+}
---
+++ app/src/main/java/com/colombelli/myapplication/TelaInicial.java
@@ -0,0 +1,23 @@
+package com.colombelli.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class TelaInicial extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_inicial);
+    }
+
+
+    public void calendario(View view){
+
+        Intent abrirCalendario = new Intent(this, CalendarioActivitie.class);
+        startActivity(abrirCalendario);
+
+    }
+}
