c0a53d18a2a20bfbd24f718f6913583dfb0a276f
Pedro <ph.pfiorentin@gmail.com>
2019-05-31 15:14:50 -0300

    agora tambem le uns bgl do banco de dados, ainda eh teste

--- app/src/main/java/com/colombelli/myapplication/TesteFirebase.java
+++ app/src/main/java/com/colombelli/myapplication/TesteFirebase.java
@@ -13,7 +13,9 @@ import com.google.firebase.database.*;
 public class TesteFirebase extends AppCompatActivity {
 
 
-    private DatabaseReference mDatabase;
+    private FirebaseDatabase database = FirebaseDatabase.getInstance();
+    private DatabaseReference myRef = database.getReference();
+    private DatabaseReference mPostReference;
 
     private Button bSendText;
     private static final String TAG = "TesteFirebase";
@@ -24,25 +26,50 @@ public class TesteFirebase extends AppCompatActivity {
         setContentView(R.layout.activity_teste_firebase);
 
 
-        mDatabase = FirebaseDatabase.getInstance().getReference();
         bSendText = (Button) findViewById(R.id.addString);
 
         bSendText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //aqui escreve
-                FirebaseDatabase database = FirebaseDatabase.getInstance();
-                DatabaseReference myRef = database.getReference("message");
-                bSendText.setText("ativado");
-                myRef.setValue("Hello, World!");
 
+                bSendText.setText("ativado");
+                basicReadWrite();
             }
         });
 
     }
 
+    public void basicReadWrite() {
+        mPostReference = FirebaseDatabase.getInstance().getReference().child("message");
+        myRef.child("moofofo").setValue("Nao aparece");
+        myRef.child("message").setValue("Aparece");
+        // [START read_message]
+        // Read from the database
 
+        ValueEventListener testeListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d(TAG, "Value is: lit bruhhh moment " + value );
+            }
 
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        };
+
+        myRef.child("message").addValueEventListener(testeListener);
+        // [END read_message]
+    }
 }
+
+
+
+
 
 
